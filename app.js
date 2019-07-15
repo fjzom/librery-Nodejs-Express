3f16556ef440d138f086ad45abbf21e24277123e
Oviedo Maltez <f.oviedo.maltez@accenture.com>
2019-07-15 12:50:43 -0500

    Eslint and views

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express =  require('express');
-
+var chalk =  require('chalk');
+var debug = require('debug')('app');
+var morgan =require('morgan');
+var path = require('path');
 var app =  express();
 
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('css', express.static(path.join(__dirname, '/node_modules/boostrap/dist/css')));
+app.use('js', express.static(path.join(__dirname, '/node_modules/boostrap/dist/js')));
+app.use('js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
 app.get('/', function(req, res){
-    console.log('');
+    res.sendFile(path.join(__dirname, 'views/index.html') );
 })
 
 
-app.listen(3000);
+app.listen(3000, function(){
+    debug( `listen info on port ${chalk.green('3000')} `);
+    console.log( `listen info on port ${chalk.green('3000')} `);
+});
